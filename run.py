32443f854eed8687522346ca9acd4be6198ae584
Karinna Rocha - NurVIX <75399228+karinnarocha@users.noreply.github.com>
2023-12-05 20:42:26 -0500

    Update run.py

--- run.py
+++ run.py
@@ -3,4 +3,4 @@ from Backend import create_app
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(host='192.168.1.57', port=8080, debug=True)
+    app.run(host="0.0.0.0", port=Config.PORT) 
